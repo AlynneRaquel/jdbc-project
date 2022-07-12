2c1dad4dd5d8fa7a86bcddd4f0c4dcddb082f067
Alynne Raquel <alynneraquel@hotmail.com>
2022-07-12 13:55:08 -0300

    Update data

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -14,7 +14,36 @@ public class Program {
 
 	public static void main(String[] args) {
 
-/* INSERINDO DADOS - INÍCIO*/		
+		Connection conn = null;
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ?"
+					+ "WHERE "
+					+ "(DepartmentID = ?)");
+			
+			st.setDouble(1, 200);
+			st.setInt(2, 2);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
+		
+		
+		
+/* INSERINDO DADOS - INÍCIO	
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
 		Connection conn = null;
@@ -61,7 +90,7 @@ public class Program {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
-		
+ INSERINDO DADOS - FIM */			
 		
 		
 /* RECUPERANDO DADOS - INÍCIO
