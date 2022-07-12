df2949afe6b81d30f3e4b0bcc063d8bb7627c025
Alynne Raquel <alynneraquel@hotmail.com>
2022-07-12 13:42:03 -0300

    Insert dados

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,9 +1,12 @@
 package aplication;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -11,10 +14,61 @@ public class Program {
 
 	public static void main(String[] args) {
 
+/* INSERINDO DADOS - INÍCIO*/		
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
 		Connection conn = null;
-		//Comando SQL
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+			
+	
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);//opcional, para retornar o id da inserção
+			
+			st.setString(1, "Carl Purple");
+			st.setString(2, "carl@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+			st.setDouble(4, 3000.0);
+			st.setInt(5, 4);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! id =" + id);
+				}
+			}
+			else {
+				System.out.println("No rows affected");
+			}
+			
+			//System.out.println("Done! Rows affected: " + rowsAffected);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ParseException e) {
+			e.printStackTrace();
+		}
+		finally{
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
+		
+		
+/* RECUPERANDO DADOS - INÍCIO
+		Connection conn = null;
+		//Comando SQL p receber consulta SQL
 		Statement st = null;
-		//obj com resultado da consulta
+		//obj q guarda resultado da consulta
 		ResultSet rs = null;
 		
 		try {
@@ -36,6 +90,7 @@ public class Program {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
+RECUPERANDO DADOS - FIM */		
 	}
 
 }
